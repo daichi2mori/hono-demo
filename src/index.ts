6ea99d38d8fc9070c9b67a1e83b1d89aafb2bc9e
daichi2mori <daichi2mori@gmail.com>
2024-02-25 14:30:24 +0900

    とりあえず認証つけた

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 import bearer from "./bearer/bearer";
 import basic from "./basic/basic";
+import { basicAuth } from "hono/basic-auth";
 
-const app = new Hono()
+const app = new Hono();
+
+// とりあえずつけとく
+app.use(
+  "/*",
+  basicAuth({
+    username: "gZW90yRhVy4Y",
+    password: "UOGgThpgzLL5",
+  })
+);
 
 app.route("/basic", basic);
 app.route("/bearer", bearer);
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+export default app;
