66aa9e113b060aac70b67cf2917749182e553ea1
daichi2mori <daichi2mori@gmail.com>
2024-04-21 02:06:29 +0900

    🎨 フォルダ構成変更

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,14 @@
 import { Hono } from "hono";
-import bearer from "./bearer/bearer";
-import basic from "./basic/basic";
-import { basicAuth } from "hono/basic-auth";
+import bearer from "./bearer";
+import basic from "./basic";
 
 const app = new Hono();
-
-// とりあえずつけとく
-app.use(
-  "/*",
-  basicAuth({
-    username: "gZW90yRhVy4Y",
-    password: "UOGgThpgzLL5",
-  })
-);
-
-app.route("/basic", basic);
-app.route("/bearer", bearer);
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.route("/basic", basic);
+app.route("/bearer", bearer);
 
 export default app;
